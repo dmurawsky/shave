828a9dbf2f3cdda83f7f68c64b02ccbfc8ed6aba
Daniel Murawsky <d.r.murawsky@gmail.com>
2017-10-06 16:13:40 -0400

    backup

--- components/Header.js
+++ components/Header.js
@@ -28,12 +28,12 @@ class Header extends React.Component {
           <div className="navbar-menu" id="superNavLeft">
             <a className="navbar-item">Today's Deals</a>
             <a className="navbar-item">Find a Store</a>
-            <a className="navbar-item">Barber Services</a>
-            <a className="navbar-item">Support</a>
+            <a className="navbar-item">Barber Shop</a>
+            <a className="navbar-item">Customer Service</a>
           </div>
           <div className="navbar-menu">
             <div className="navbar-end">
-              <a className="navbar-item" style={{marginRight: 160}}>Sign In | Register</a>
+              <a className="navbar-item" style={{marginRight: 141}}>Sign In | Register</a>
               <Cart />
             </div>
           </div>
@@ -54,16 +54,17 @@ class Header extends React.Component {
               <span id="searchSelect" className="select">
                 <select ref={(select) => this.select = select} onChange={this._onChange}>
                   <option>All</option>
-                  <option>3 Blade Razor</option>
-                  <option>5 Blade Razor</option>
-                  <option>After-Shave</option>
-                  <option>Blades</option>
-                  <option>Electric Shavers</option>
-                  <option>Fragrances</option>
-                  <option>Razor Stands</option>
-                  <option>Safety Razor</option>
-                  <option>Shaving Brush</option>
-                  <option>Shaving Cream</option>
+                  <option>Shave Care</option>
+                  <option>Hair Care</option>
+                  <option>Beard Care</option>
+                  <option>Nail Care</option>
+                  <option>Skin Care</option>
+                  <option>Shave Hardware</option>
+                  <option>Electric Hardware</option>
+                  <option>Maintenance</option>
+                  <option>Accessories</option>
+                  <option>Blade Refills</option>
+                  <option>Leather Goods</option>
                 </select>
               </span>
             </p>
@@ -84,19 +85,20 @@ class Header extends React.Component {
         </nav>
         <nav id="subNav" className="navbar" role="navigation" aria-label="main navigation">
           <div className="navbar-menu">
-            <div id="categoriesNav" className="navbar-item has-dropdown is-hoverable">
+            <div id="categoriesNav" className="sub-nav-dropdown navbar-item has-dropdown is-hoverable">
               <a className="navbar-link is-active">Categories</a>
               <div className="navbar-dropdown is-boxed">
-                <a className="navbar-item">3 Blade Razor</a>
-                <a className="navbar-item">5 Blade Razor</a>
-                <a className="navbar-item">After-Shave</a>
-                <a className="navbar-item">Blades</a>
-                <a className="navbar-item">Electric Shavers</a>
-                <a className="navbar-item">Fragrances</a>
-                <a className="navbar-item">Razor Stands</a>
-                <a className="navbar-item">Safety Razor</a>
-                <a className="navbar-item">Shaving Brush</a>
-                <a className="navbar-item">Shaving Cream</a>
+                <a className="navbar-item">Shave Care</a>
+                <a className="navbar-item">Hair Care</a>
+                <a className="navbar-item">Beard Care</a>
+                <a className="navbar-item">Nail Care</a>
+                <a className="navbar-item">Skin Care</a>
+                <a className="navbar-item">Shave Hardware</a>
+                <a className="navbar-item">Electric Hardware</a>
+                <a className="navbar-item">Maintenance</a>
+                <a className="navbar-item">Accessories</a>
+                <a className="navbar-item">Blade Refills</a>
+                <a className="navbar-item">Leather Goods</a>
               </div>
             </div>
             <a className="navbar-item"><span>SHAVE CARE</span></a>
@@ -107,11 +109,51 @@ class Header extends React.Component {
             <a className="navbar-item"><span>TRAVEL</span></a>
             <a className="navbar-item"><span>GIFT IDEAS</span></a>
           </div>
+          <div className="navbar-menu">
+            <div className="navbar-end">
+              <div id="accountNav" className="sub-nav-dropdown navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link is-active">My Account</a>
+                <div className="navbar-dropdown is-boxed">
+                  <a className="navbar-item">Your Account</a>
+                  <a className="navbar-item">Your Orders</a>
+                  <a className="navbar-item">Your Saved Items</a>
+                  <div className="navbar-divider" />
+                  <a className="navbar-item">Brotherhood Rewards</a>
+                  <a className="navbar-item">Account Services</a>
+                  <a className="navbar-item">Replenishment Service</a>
+                  <a className="navbar-item">Box Delivery Service</a>
+                  <a className="navbar-item">Education Center</a>
+                  <a className="navbar-item">American Shave Mailer</a>
+                  <a className="navbar-item">Notification Settings</a>
+                  <div className="navbar-divider" />
+                  <a className="navbar-item">Sign Out</a>
+                </div>
+              </div>
+            </div>
+          </div>
         </nav>
       </div>
     )
   }
 }
+
+
+// <div className="navbar-item has-dropdown is-hoverable">
+//   <a className="navbar-link is-active" style={{padding:0}}>Professional</a>
+//   <div className="navbar-dropdown is-boxed">
+//     <a className="navbar-item">Shave Care</a>
+//     <a className="navbar-item">Hair Care</a>
+//     <a className="navbar-item">Beard Care</a>
+//     <a className="navbar-item">Nail Care</a>
+//     <a className="navbar-item">Skin Care</a>
+//     <a className="navbar-item">Shave Hardware</a>
+//     <a className="navbar-item">Electric Hardware</a>
+//     <a className="navbar-item">Maintenance</a>
+//     <a className="navbar-item">Accessories</a>
+//     <a className="navbar-item">Blade Refills</a>
+//     <a className="navbar-item">Leather Goods</a>
+//   </div>
+// </div>
 
 
 export default Header;
